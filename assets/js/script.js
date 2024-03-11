ddde2700dbea00418bce07523831b8b4d496fe80
Caleb Hammer <hammer.calebr@gmail.com>
2024-03-11 13:15:24 -0400

    added comments to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,82 @@
-$(document).ready(function () {
+// THIS IS THE CODE THAT INTERACTS WITH THE DOM IN A CALL TO JQUERY TO ENSURE THAT THE 
+// THE CODE DOESN'T RUN UNTIL THE BROSWER  HAS FINISHED RENDERING ALL THE ELEMENTS IN THE HTML.
+  $(document).ready(function () {
 
-      $("#currentDay").text(moment().format("dddd, MMM DD, YYYY - h:mm:ss a")); 
-    
-      var modal = document.getElementById("myModal");
-      var span = document.getElementsByClassName("sayNo")[0];
-      var clear = document.getElementsByClassName("sayYes")[0];
-      var btn = document.getElementById("clearTask");
-    
-      btn.onclick = function () {modal.style.display = "block";};
-    
-      window.onclick = function (event) {
-      if (event.target == modal) {modal.style.display = "none";}
-      };
-    
-      span.onclick = function () {modal.style.display = "none";};
-    
-      clear.onclick = function () {modal.style.display = "none";
-      localStorage.clear();
-      window.location.reload();
-      };
-      
-      $(".saveBtn").on("click", function () {
-        var content = $(this).siblings(".description").val(); 
-        var timeStamp = $(this).parent().attr("id");
-    
-      localStorage.setItem(timeStamp, content);
+// THIS IS THE CODE TO DISPLAY THE CURRENT DATE AND TIME IN THE HEADER OF THE PAGE, WHICH IS USING MOMENT.JS
+  $("#currentDay").text(moment().format("dddd, MMM DD, YYYY - h:mm:ss a")); 
+
+// VARIABLES TO CLEAR TASKS BUTTON
+  var modal = document.getElementById("myModal");
+  var span = document.getElementsByClassName("sayNo")[0];
+  var clear = document.getElementsByClassName("sayYes")[0];
+  var btn = document.getElementById("clearTask");
+
+// FUNCTIONS FOR ONCLICK WHEN USER INTERACTS WITH CLEAR TASKS BUTTON 
+// ON CLICK, THE MODAL OPENS WHEN USER CLICKS CLEAR MY TASKS AN DOPENS MODAL.
+  btn.onclick = function () {modal.style.display = "block";};
+
+// ON CLICK, WHEN A USER CLICK OUTSIDE OF THE MODAL PROMPT, THE MODAL CLOSES.
+  window.onclick = function (event) {
+  if (event.target == modal) {modal.style.display = "none";}
+  };
+
+// ON CLICK, USER CLICKS NO, THE MODAL CLOSES
+  span.onclick = function () {modal.style.display = "none";};
+
+// USER CLICKS YES, LOCAL STORAGE CLEARS AND RELOADS PAGE.
+  clear.onclick = function () {modal.style.display = "none";
+  localStorage.clear();
+  window.location.reload();
+  };
+  
+// THIS IS THE CODE FOR CLICK LISTENER WHEN USER HITS THE "saveBtn" button. 
+  $(".saveBtn").on("click", function () {
+    var content = $(this).siblings(".description").val(); 
+    var timeStamp = $(this).parent().attr("id");
+
+// THIS IS THE CODE TO GET ANY USER INPUT THAT WAS SAVE IN LOCALE STORAGE AND WILL 
+// SET THE VALUES OF THE CORRESPONDING TEXT AREA ELEMENTS//.
+  localStorage.setItem(timeStamp, content);
+  })
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+  $("#hour18 .description").val(localStorage.getItem("hour18"));
+
+// THIS IS THE CODE THAT WILL APPLY PAST, PRESENT AND/OR FUTURE CLASS TO
+// EACH TIME BLOCK BY COMPARING THE ID TO THE CURRENT YEAR.
+  function trackTime() {
+  // VARIABLE TO GET THE CUURENT HOUR, USING MOMEMT.JS
+  var currentHour = moment().hour(); 
+
+// FOR EACH LOOP AT EVERY HOUR
+  $(".time-block").each(function () {
+    var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+// IF STATEMENTS FOR WHEN THE TIME HAS PAST THE PRESENT HOUR, COLOUR BLOCK ACCORDINGLY
+  if (timeBlock < currentHour) {
+    $(this).addClass("past");
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+    }
+  else if (timeBlock === currentHour) {
+    $(this).removeClass("past");
+    $(this).addClass("present");
+    $(this).removeClass("future");
+    }
+  else {
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future");
+    }
       })
-      $("#hour9 .description").val(localStorage.getItem("hour9"));
-      $("#hour10 .description").val(localStorage.getItem("hour10"));
-      $("#hour11 .description").val(localStorage.getItem("hour11"));
-      $("#hour12 .description").val(localStorage.getItem("hour12"));
-      $("#hour13 .description").val(localStorage.getItem("hour13"));
-      $("#hour14 .description").val(localStorage.getItem("hour14"));
-      $("#hour15 .description").val(localStorage.getItem("hour15"));
-      $("#hour16 .description").val(localStorage.getItem("hour16"));
-      $("#hour17 .description").val(localStorage.getItem("hour17"));
-      $("#hour18 .description").val(localStorage.getItem("hour18"));
-    
-      function trackTime() {
-      var currentHour = moment().hour(); 
-    
-      $(".time-block").each(function () {
-        var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
-    
-      if (timeBlock < currentHour) {
-        $(this).addClass("past");
-        $(this).removeClass("future");
-        $(this).removeClass("present");
-        }
-      else if (timeBlock === currentHour) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
-        $(this).removeClass("future");
-        }
-      else {
-        $(this).removeClass("present");
-        $(this).removeClass("past");
-        $(this).addClass("future");
-        }
-          })
-      }
-    //REPEAT THE TRACKER
-      trackTime(); 
-    })
-    
+  }
+//REPEAT THE TRACKER
+  trackTime(); 
+})
